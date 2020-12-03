b430dc6b57d6f1a96a64baf32633a0a727ab5bad
Antonio Vivace <antonio.vivace@cern.ch>
2020-12-03 12:29:03 +0100

    Copy GA Actions templates

--- automation_tools/scripts/ga-migration/main.py
+++ automation_tools/scripts/ga-migration/main.py
@@ -4,6 +4,7 @@ import logging
 import re
 import os
 
+import requests
 import click
 
 logging.basicConfig(level=logging.DEBUG)
@@ -27,7 +28,9 @@ def replace_simple(text, replacing, filepath):
     """
     Replaces every match of a string with another in the specified file
     """
-    logging.info("TASK: Simple replacing %s with %s in %s" % (text, replacing, filepath))
+    logging.info(
+        "TASK: Simple replacing %s with %s in %s" % (text, replacing, filepath)
+    )
     if os.path.isfile(filepath):
         logging.info("Found %s" % filepath)
         # TODO: expose number of matches
@@ -42,7 +45,9 @@ def replace_regex(regex, output, filepath):
     """
     Replaces every match of a string with another in the specified file
     """
-    logging.info("TASK: RegEx replacing %s with %s in %s" % (regex, output, filepath))
+    logging.info(
+        "TASK: RegEx replacing %s with %s in %s" % (regex, output, filepath)
+    )
     if os.path.isfile(filepath):
         logging.info("Found %s" % filepath)
         # TODO: expose number of matches
@@ -51,6 +56,16 @@ def replace_regex(regex, output, filepath):
                 print(re.sub(regex, output, line), end="")
     else:
         logging.info("SKIPPED TASK. No %s found" % filepath)
+
+
+def download_file(url, destination):
+    # Get path
+    dirname = os.path.dirname(os.path.realpath(destination))
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    print(dirname)
+    r = requests.get(url, allow_redirects=True)
+    open(destination, "wb").write(r.content)
 
 
 @click.command()
@@ -64,7 +79,9 @@ def pipeline(targetpath):
     # Reference: https://codimd.web.cern.ch/TOOkF5yhSAKJq3TiY0L42A?view
 
     # Step 3
-    replace_simple(targetpath + ".travis.yml", ".github/workflows/*.yml", ".editorconfig")
+    replace_simple(
+        targetpath + ".travis.yml", ".github/workflows/*.yml", ".editorconfig"
+    )
     # Step 4.1
     replace_regex(
         r"https:\/\/img\.shields\.io\/travis\/([a-z]*\/[a-z-]*)\.svg",
@@ -94,6 +111,18 @@ def pipeline(targetpath):
 
     # Remove bak files
     delete_file(targetpath + "*.bak")
+
+    # Download tests.yml template
+    download_file(
+        "https://raw.githubusercontent.com/inveniosoftware/.github/master/workflow-templates/tests.yml",
+        targetpath + ".github/workflows/tests.yml",
+    )
+
+    # Download pypi-publish.yml template
+    download_file(
+        "https://raw.githubusercontent.com/inveniosoftware/.github/master/workflow-templates/pypi-publish.yml",
+        targetpath + ".github/workflows/pypi-publish.yml",
+    )
 
 
 if __name__ == "__main__":
