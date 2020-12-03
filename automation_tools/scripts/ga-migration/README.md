88c97f965f94f7f92c38b15de7cf5b1a1f4fca52
Marco Vidal García <marco.vidal@cern.ch>
2020-12-03 12:06:23 +0100

    global: several improvements
    - rename functions to snake case according to Invenio dev guidelines
    - `delete_file` now permits using `*`
    - fix check-manifest replacement
    - remove *.bak files at the end

---
+++ automation_tools/scripts/ga-migration/README.md
@@ -0,0 +1,13 @@
+# ga-migration
+
+## Install
+
+```console
+$ pip install -r requirements.txt
+```
+
+## Run
+
+```console
+$ python main.py --targetpath ../path/to/invenio/repo
+```
--- automation_tools/scripts/ga-migration/main.py
+++ automation_tools/scripts/ga-migration/main.py
@@ -1,26 +1,29 @@
-import os
-import logging
 import fileinput
+import glob
+import logging
 import re
+import os
+
 import click
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def deleteFile(filepath):
+def delete_file(filepath):
     """
     If a file exists, delete it
     """
     logging.info("TASK: Deleting %s" % filepath)
-    if os.path.isfile(filepath):
-        logging.info("Found %s" % filepath)
-        os.remove(filepath)
-        logging.info("Deleted %s" % filepath)
-    else:
-        logging.info("No %s found" % filepath)
+    for file in glob.glob(filepath):
+        if os.path.isfile(file):
+            logging.info("Found %s" % file)
+            os.remove(file)
+            logging.info("Deleted %s" % file)
+        else:
+            logging.info("No %s found" % file)
 
 
-def replaceSimple(text, replacing, filepath):
+def replace_simple(text, replacing, filepath):
     """
     Replaces every match of a string with another in the specified file
     """
@@ -35,7 +38,7 @@ def replaceSimple(text, replacing, filepath):
         logging.info("No %s found" % filepath)
 
 
-def replaceRE(regex, output, filepath):
+def replace_regex(regex, output, filepath):
     """
     Replaces every match of a string with another in the specified file
     """
@@ -59,33 +62,38 @@ def pipeline(targetpath):
     # TODO: add the trailing slash only if needed
     targetpath = targetpath + "/"
     # Reference: https://codimd.web.cern.ch/TOOkF5yhSAKJq3TiY0L42A?view
-    # Step 2
-    deleteFile(targetpath + ".travis.yml")
+
     # Step 3
-    replaceSimple(targetpath + ".travis.yml", ".github/workflows/*.yml", ".editorconfig")
+    replace_simple(targetpath + ".travis.yml", ".github/workflows/*.yml", ".editorconfig")
     # Step 4.1
-    replaceRE(
+    replace_regex(
         r"https:\/\/img\.shields\.io\/travis\/([a-z]*\/[a-z-]*)\.svg",
         "https://github.com/\\1/workflows/CI/badge.svg",
         targetpath + "README.rst",
     )
-    replaceRE(
+    replace_regex(
         r"https:\/\/travis-ci\.org\/([a-z]*\/[a-z-]*)",
         "https://github.com/\\1/actions?query=workflow%3ACI",
         targetpath + "README.rst",
     )
     # Step 4.2
-    replaceRE(
-        r"https:\/\/travis-ci\.com\/([a-z]*\/[a-z-]*)\/pull_requests",
-        "https://github.com/\\1/actions?query=event%3Apull_request",
+    replace_regex(
+        r"https:\/\/travis-ci\.(org|com)\/([a-z]*\/[a-z-]*)\/pull_requests",
+        "https://github.com/\\2/actions?query=event%3Apull_request",
         targetpath + "CONTRIBUTING.rst",
     )
     # Step 5
-    replaceSimple(
-        'check_manifest --ignore ".travis-*"',
-        'check_manifest --ignore ".*-requirements.txt"',
+    replace_simple(
+        'check-manifest --ignore ".travis-*"',
+        'check-manifest --ignore ".*-requirements.txt"',
         targetpath + "run-tests.sh",
     )
+
+    # Delete travis file
+    delete_file(targetpath + ".travis.yml")
+
+    # Remove bak files
+    delete_file(targetpath + "*.bak")
 
 
 if __name__ == "__main__":
