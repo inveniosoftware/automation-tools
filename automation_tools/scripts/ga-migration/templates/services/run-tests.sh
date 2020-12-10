3bf0d14e168bba794d59b92d6324b80e92a93dcc
Marco Vidal García <marco.vidal@cern.ch>
2020-12-10 09:00:54 +0100

    ga-migration: replace es with elasticsearch

--- automation_tools/scripts/ga-migration/templates/services/run-tests.sh
+++ automation_tools/scripts/ga-migration/templates/services/run-tests.sh
@@ -22,7 +22,7 @@ trap cleanup EXIT
 
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-eval "$(docker-services-cli up {{ "--db ${DB:-postgresql} " if db }}{{ "--search ${SEARCH:-es} " if search }}{{ "--cache ${CACHE:-redis} " if cache }}{{ "--mq ${MQ:-rabbitmq} " if mq }}--env)"
+eval "$(docker-services-cli up {{ "--db ${DB:-postgresql} " if db }}{{ "--search ${SEARCH:-elasticsearch} " if search }}{{ "--cache ${CACHE:-redis} " if cache }}{{ "--mq ${MQ:-rabbitmq} " if mq }}--env)"
 python -m pytest
 tests_exit_code=$?
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
